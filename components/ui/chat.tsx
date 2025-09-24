7588beb24e7f124b90d2325e95e0b55c5d7eefd0
medprod <medhaa.prodduturi@gmail.com>
2025-09-24 00:12:46 -0500

    Push all changes

--- components/ui/chat.tsx
+++ components/ui/chat.tsx
@@ -196,7 +196,7 @@ export function Chat({
     <ChatContainer className={className}>
       {isEmpty && append && suggestions ? (
         <PromptSuggestions
-          label="Try these prompts ✨"
+          label="Try these prompts 🚀"
           append={append}
           suggestions={suggestions}
         />
